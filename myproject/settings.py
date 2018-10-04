cbf0de59bd815d49fb33beb4d126a807bdfe05f6
xghijk <xghijk@qq.com>
2018-10-04 15:03:30 +0800

    my django app,second commit

--- myproject/settings.py
+++ myproject/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'p)b93#&((d96l@qg^4%t)lr0wg8hm5k&p3jsk82i=%kmozpqc%'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['SHARE.pythonanywhere.com',]
 
 
 # Application definition
